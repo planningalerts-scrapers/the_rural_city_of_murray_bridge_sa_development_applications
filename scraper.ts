748dcc441482e18f317e84ec70c3294158a66c04
MichaelBone <Michael.Bone@senet.com.au>
2020-07-30 17:27:28 +0930

    Updated the URL and selector.

--- scraper.ts
+++ scraper.ts
@@ -27,7 +27,7 @@ import * as didyoumean from "didyoumean2";
 
 sqlite3.verbose();
 
-const DevelopmentApplicationsUrl = "http://www.murraybridge.sa.gov.au/page.aspx?u=1022";
+const DevelopmentApplicationsUrl = "https://www.murraybridge.sa.gov.au/services/develop-plan-build/developmentandplanning/development-assessment-register?result_57806_result_page={0}";
 const CommentUrl = "mailto:council@murraybridge.sa.gov.au";
 
 declare const global: any;
@@ -55,7 +55,7 @@ async function initializeDatabase() {
 
 async function insertRow(database, developmentApplication) {
     return new Promise((resolve, reject) => {
-        let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?, ?)");
+        let sqlStatement = database.prepare("insert or replace into [data] values (?, ?, ?, ?, ?, ?, ?, ?)");
         sqlStatement.run([
             developmentApplication.applicationNumber,
             developmentApplication.address,
@@ -70,10 +70,7 @@ async function insertRow(database, developmentApplication) {
                 console.error(error);
                 reject(error);
             } else {
-                if (this.changes > 0)
-                    console.log(`    Inserted: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\", description \"${developmentApplication.description}\", legal description \"${developmentApplication.legalDescription}\" and received date \"${developmentApplication.receivedDate}\" into the database.`);
-                else
-                    console.log(`    Skipped: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\", description \"${developmentApplication.description}\", legal description \"${developmentApplication.legalDescription}\" and received date \"${developmentApplication.receivedDate}\" because it was already present in the database.`);
+                console.log(`    Saved application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\", description \"${developmentApplication.description}\", legal description \"${developmentApplication.legalDescription}\" and received date \"${developmentApplication.receivedDate}\" to the database.`);
                 sqlStatement.finalize();  // releases any locks
                 resolve(row);
             }
@@ -1109,27 +1106,32 @@ async function main() {
 
     readAddressInformation();
 
-    // Retrieve the page that contains the links to the PDFs.
+    // Retrieve the pages that contain the links to the PDFs.
 
-    console.log(`Retrieving page: ${DevelopmentApplicationsUrl}`);
-
-    let body = await request({ url: DevelopmentApplicationsUrl, proxy: process.env.MORPH_PROXY });
-    await sleep(2000 + getRandom(0, 5) * 1000);
-    let $ = cheerio.load(body);
-    
     let pdfUrls: string[] = [];
-    for (let element of $("td.uContentListDesc a").get()) {
-        let pdfUrl = new urlparser.URL(element.attribs.href, DevelopmentApplicationsUrl);
-        pdfUrl.protocol = "http";  // force to use HTTP instead of HTTPS
-        if (pdfUrl.href.toLowerCase().includes(".pdf"))
-            if (!pdfUrls.some(url => url === pdfUrl.href))  // avoid duplicates
-                pdfUrls.push(pdfUrl.href);
+
+    for (let index = 1; index <= 10; index++) {  // search up to 10 pages
+        let url = DevelopmentApplicationsUrl.replace(/\{0\}/g, index.toString());
+        console.log(`Retrieving page: ${url}`);
+
+        let body = await request({ url: url, proxy: process.env.MORPH_PROXY });
+        await sleep(2000 + getRandom(0, 5) * 1000);
+        let $ = cheerio.load(body);
+        
+        for (let element of $("h3.generic-list__title a").get()) {
+            let pdfUrl = new urlparser.URL(element.attribs.href, DevelopmentApplicationsUrl);
+            pdfUrl.protocol = "http";  // force to use HTTP instead of HTTPS
+            if (pdfUrl.href.toLowerCase().includes(".pdf"))
+                if (!pdfUrls.some(url => url === pdfUrl.href))  // avoid duplicates
+                    pdfUrls.push(pdfUrl.href);
+        }
     }
 
     if (pdfUrls.length === 0) {
         console.log("No PDF URLs were found on the page.");
         return;
     }
+    console.log(`Found ${pdfUrls.length} PDF URL(s).`);
 
     // Select the most recent PDF.  And randomly select one other PDF (avoid processing all PDFs
     // at once because this may use too much memory, resulting in morph.io terminating the current
